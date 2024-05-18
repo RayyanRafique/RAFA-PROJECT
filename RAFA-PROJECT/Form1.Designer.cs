55bbf72f909a5bb25c620d33ad351b37d434e084
Fahira <fahira.ash@gmail.com>
2024-05-19 01:54:17 +0500

    New pages added

--- RAFA-PROJECT/Form1.Designer.cs
+++ RAFA-PROJECT/Form1.Designer.cs
@@ -29,35 +29,23 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.emailTB = new System.Windows.Forms.RichTextBox();
             this.passwordTB = new System.Windows.Forms.RichTextBox();
             this.signInButton = new System.Windows.Forms.Button();
             this.signUpButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(100, 130);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(221, 89);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // emailTB
             // 
             this.emailTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
             this.emailTB.Font = new System.Drawing.Font("Leelawadee UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.emailTB.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.emailTB.Location = new System.Drawing.Point(42, 350);
+            this.emailTB.Location = new System.Drawing.Point(112, 835);
             this.emailTB.Margin = new System.Windows.Forms.Padding(0);
             this.emailTB.Name = "emailTB";
-            this.emailTB.Size = new System.Drawing.Size(340, 58);
+            this.emailTB.Size = new System.Drawing.Size(900, 133);
             this.emailTB.TabIndex = 1;
             this.emailTB.Text = "Email";
             this.emailTB.TextChanged += new System.EventHandler(this.EmailTextBox_TextChanged);
@@ -69,10 +57,10 @@
             this.passwordTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
             this.passwordTB.Font = new System.Drawing.Font("Leelawadee UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.passwordTB.ForeColor = System.Drawing.SystemColors.ControlDark;
-            this.passwordTB.Location = new System.Drawing.Point(42, 464);
+            this.passwordTB.Location = new System.Drawing.Point(112, 1106);
             this.passwordTB.Margin = new System.Windows.Forms.Padding(0);
             this.passwordTB.Name = "passwordTB";
-            this.passwordTB.Size = new System.Drawing.Size(340, 58);
+            this.passwordTB.Size = new System.Drawing.Size(900, 133);
             this.passwordTB.TabIndex = 2;
             this.passwordTB.Text = "Password";
             this.passwordTB.TextChanged += new System.EventHandler(this.PasswordTextBox_TextChanged);
@@ -86,9 +74,10 @@
             this.signInButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.signInButton.Font = new System.Drawing.Font("Leelawadee UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.signInButton.ForeColor = System.Drawing.SystemColors.Control;
-            this.signInButton.Location = new System.Drawing.Point(62, 594);
+            this.signInButton.Location = new System.Drawing.Point(165, 1416);
+            this.signInButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.signInButton.Name = "signInButton";
-            this.signInButton.Size = new System.Drawing.Size(130, 45);
+            this.signInButton.Size = new System.Drawing.Size(347, 107);
             this.signInButton.TabIndex = 3;
             this.signInButton.Text = "Sign In";
             this.signInButton.UseVisualStyleBackColor = false;
@@ -101,26 +90,39 @@
             this.signUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.signUpButton.Font = new System.Drawing.Font("Leelawadee UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.signUpButton.ForeColor = System.Drawing.SystemColors.Control;
-            this.signUpButton.Location = new System.Drawing.Point(232, 594);
+            this.signUpButton.Location = new System.Drawing.Point(619, 1416);
+            this.signUpButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.signUpButton.Name = "signUpButton";
-            this.signUpButton.Size = new System.Drawing.Size(130, 45);
+            this.signUpButton.Size = new System.Drawing.Size(347, 107);
             this.signUpButton.TabIndex = 4;
             this.signUpButton.Text = "Sign Up";
             this.signUpButton.UseVisualStyleBackColor = false;
             this.signUpButton.Click += new System.EventHandler(this.SignUpButton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(258, 374);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(589, 212);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(36)))), ((int)(((byte)(44)))));
-            this.ClientSize = new System.Drawing.Size(414, 811);
+            this.ClientSize = new System.Drawing.Size(1104, 1934);
             this.Controls.Add(this.signUpButton);
             this.Controls.Add(this.signInButton);
             this.Controls.Add(this.passwordTB);
             this.Controls.Add(this.emailTB);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
---
+++ RAFA-PROJECT/MainPage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RAFA_PROJECT
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Home home = new Home();
+            home.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Settings settings = new Settings();
+            settings.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Menu menu = new Menu();
+            menu.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Store store = new Store();
+            store.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Reminder reminder = new Reminder();
+            reminder.Show();
+            this.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Recommendation recomm = new Recommendation();
+            recomm.Show();
+            this.Hide();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Appointment appoint = new Appointment();
+            appoint.Show();
+            this.Hide();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
